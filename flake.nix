6b343c9d1ead44b112b21ad94893b7d8f3f6e856
iamlyth <lytha@me.com>
2025-12-17 01:08:13 -0500

    nix-darwin integrated into flake

--- flake.nix
+++ flake.nix
@@ -3,20 +3,21 @@
 
   # Inputs
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     vpn-confinement.url = "github:Maroka-chan/VPN-Confinement";
-		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    lanzaboote.url = "github:nix-community/lanzaboote/v0.4.3";
+    darwin.url = "github:lnl7/nix-darwin/nix-darwin-25.11";
+    darwin.inputs.nixpkgs.follows = "nixpkgs"; 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {self, nixpkgs, vpn-confinement, nixos-hardware, home-manager, ...
+  outputs = {self, nixpkgs, vpn-confinement, nixos-hardware, lanzaboote, home-manager, ...
   }@inputs: {
     nixosConfigurations = {
-      mOS = let
-        system = "x86_64-linux";
-
+      mOS = let system = "x86_64-linux";
       in nixpkgs.lib.nixosSystem {
         modules = [
           home-manager.nixosModules.home-manager {
@@ -51,6 +52,7 @@
       in nixpkgs.lib.nixosSystem {
         modules = [
 					nixos-hardware.nixosModules.framework-desktop-amd-ai-max-300-series
+					lanzaboote.nixosModules.lanzaboote
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
@@ -63,7 +65,37 @@
         ];
       };
 
-	  
+      cOS = let system = "x86_64-linux";
+      in nixpkgs.lib.nixosSystem {
+        modules = [
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.lalobied = import ./home-manager/home.nix;
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+            };
+          }
+          ./hosts/cOS.nix
+        ];
+      };
     };
+    darwinConfigurations."lythbook3" = inputs.darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        modules = [
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.lalobied = import ./home-manager/homeawayfromhome.nix;
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+            };
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+	  			./hosts/aOS.nix
+        ];
+    }; 
   };
 }
