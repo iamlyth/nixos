8329838be6761e6a8aeedcc877cdf1b1d0d0d47f
lyth <lytha@me.com>
2026-01-28 23:13:26 -0500

    Setting up FTP Dropbox for Printer
    With a cron job

--- containers/paperLXC.nix
+++ containers/paperLXC.nix
@@ -1,5 +1,9 @@
 #containers/photoOS.nix
 { modulesPath, config, lib, pkgs, ... }:
+let
+	ftpDir = "/var/ftp";
+	printerDir = "/var/ftp/printer";
+in
 {
 	imports = [
 		../modules/paper.nix
@@ -50,9 +54,15 @@
 		environment.systemPackages = with pkgs;  [
 			git
 			cifs-utils
-			eza
+			openssl
 		];
-		networking.firewall.allowedTCPPorts = [28981];
+		networking.firewall.allowedTCPPorts = [
+			28981	#paperless
+			21		#ftp
+		];
+		networking.firewall.allowedTCPPortRanges = [
+  		{ from = 51000; to = 51999; }
+		];
 		papermodule = {
 			enable = true;
 		};
@@ -60,7 +70,47 @@
 			enable = true;
 			port = [55];
 		};
+
+		services.vsftpd = {
+			enable = true;
+			writeEnable = true;
+			localUsers = true;
+			chrootlocalUser = true;
+			allowWriteableChroot = true;
+			forceLocalLoginsSSL = true;
+			forceLocalDataSSL = true;
+			rsaCertFile = "/var/vsftpd/vsftpd.pem";
+			extraConfig = ''
+				pasv_enable=YES
+				pasv_min_port=51000
+				pasv_max_port=51999
+				require_ssl_reuse=NO
+				ssl_ciphers=HIGH
+				seccomp_sandbox=NO
+				chmod_enable=YES
+				strict_ssl_read_eof=NO
+  		'';
+		};
+	
+		services.cron = {
+			enable = true;
+			systemCronJobs = [
+				"*/5 * * * * root ${pkgs.coreutils}/bin/chmod 775 /Users/printer/inbox/*.pdf; ${pkgs.coreutils}/bin/mv /Users/printer/inbox/*.pdf /var/lib/paperless/consume 2>&1 | logger -t paperless-move"
+			];
+		};
+
+		users.users.printer = {
+			isNormalUser = true;                # Create a normal user
+			home = "/Users/printer";            # Home directory for FTP uploads
+			shell = pkgs.bash;                  # Default shell
+			extraGroups = ["vault"];
+		};
 		
+		system.activationScripts.createFtpDirectory = ''
+			chown -R printer:vault /Users/printer
+			chmod -R 777 /Users/printer
+		'';
+
 		time.timeZone = "US/Michigan";
 
 		nix.settings.trusted-users = ["lalobied"];
