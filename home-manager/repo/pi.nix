135981a6c714e970f4c0b75d56b69618f52122be
Lyth Alobiedat <lytha@me.com>
2026-05-17 00:43:45 -0400

    Now using pi.nix flake instead of pi-coding-agent from nixpkgs

--- home-manager/repo/pi.nix
+++ home-manager/repo/pi.nix
@@ -3,8 +3,9 @@ with lib;
 let
   cfg = config.pimodule;
   jail = inputs.jaildotnix.lib.init pkgs;
+  piPackage = inputs.pi-nix.packages.${pkgs.stdenv.hostPlatform.system}.coding-agent;
 
-  piAgentDir = "${config.home.homeDirectory}/.config/pi/agent";
+  piAgentDir = "${config.home.homeDirectory}/.pi/agent";
 
   modelsJson = builtins.toJSON {
     providers = {
@@ -19,12 +20,12 @@ let
     };
   };
 
-  jailed-pi = jail "pi" pkgs.pi-coding-agent [
+  jailed-pi = jail "pi" piPackage [
     jail.combinators.network
     (jail.combinators.persist-home "pi-coder")
     (jail.combinators.try-readwrite piAgentDir)
-    (jail.combinators.set-env "PI_CODING_AGENT_DIR" piAgentDir)
-    (jail.combinators.add-pkg-deps (with pkgs; [ git fd bash gnused findutils coreutils gnugrep ripgrep gawk diffutils jq ]))
+    (jail.combinators.set-env "PI_AGENT_DIR" piAgentDir)
+    (jail.combinators.add-pkg-deps (with pkgs; [ git fd bash gnused findutils coreutils gnugrep ripgrep gawk diffutils jq nodejs ]))
     (jail.combinators.unsafe-add-raw-args ''--bind "$PWD" "/workspace/$(basename "$PWD")"'')
     (jail.combinators.unsafe-add-raw-args ''--chdir "/workspace/$(basename "$PWD")"'')
   ];
@@ -36,13 +37,15 @@ in
       default = false;
       example = true;
       description = ''
-        Whether or not to enable picoding agent
+        Whether or not to enable pi coding agent
       '';
     };
   };
+
   config = mkIf cfg.enable {
     home.packages = [ jailed-pi ];
-    home.activation.piConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+
+    home.activation.piSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       run mkdir -p "${piAgentDir}"
 
       echo '${modelsJson}' > "${piAgentDir}/models.json"
@@ -52,7 +55,7 @@ in
         echo '{}' > "$settingsFile"
       fi
       tmp=$(mktemp)
-      ${pkgs.jq}/bin/jq '. + {defaultProvider: "ollama", defaultModel: "gemma4:26b"}' "$settingsFile" > "$tmp"
+      ${pkgs.jq}/bin/jq '. + {defaultProvider: "ollama", defaultModel: "gemma4:31b"}' "$settingsFile" > "$tmp"
       run mv "$tmp" "$settingsFile"
     '';
   };
